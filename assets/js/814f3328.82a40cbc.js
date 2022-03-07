f7c50d4eb7bbc7ec2077c30ddef30241837ec2b9
hooopo <Hoooopo@gmail.com>
2022-03-07 10:30:24 +0000

    Deploying to gh-pages from  @ 8fafb3199c4dc7939a3cf669b8f81c50680a10f4 🚀

---
+++ assets/js/814f3328.82a40cbc.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocus=self.webpackChunkdocus||[]).push([[2535],{45641:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Changelog","permalink":"/blog/changelog"},{"title":"About","permalink":"/blog/about"}]}')}}]);
